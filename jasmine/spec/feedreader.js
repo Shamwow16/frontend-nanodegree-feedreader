8ba634dc09121aa92ca1d2e7d7ecef8232058769
Shamwow16 <shamyleg@gmail.com>
2016-04-12 21:50:41 -0500

    Write test for at least single entry element in feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,13 +101,29 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        var testFeedID = 0;
+        var feedContainer = $('.feed');
+        var entryArray = [];
+        beforeEach(function(done) {
+            loadFeed(testFeedID, function() {
+                entryArray = $('.entry');
+                done();
+            });
+        });
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+        it('has at least one .entry element within the .feed container', function(done) {
+            expect(entryArray.length).not.toBe(0);
+            done();
+        })
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
